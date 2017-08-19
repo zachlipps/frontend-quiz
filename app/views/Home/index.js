8b0c8b75075b2d85a64f932f4802a8a8ca076ad4
Zach Lipps <zacharylipps10@gmail.com>
2017-08-19 10:47:07 -0700

    render users from graphQL endpoint

--- app/views/Home/index.js
+++ app/views/Home/index.js
@@ -2,8 +2,43 @@ import React from 'react'
 import {
   Link,
 } from 'react-router-dom'
+import { graphql, gql } from 'react-apollo';
 
 class Home extends React.PureComponent {
+  renderUsers() {
+    if (this.props.AllUsersQuery && this.props.AllUsersQuery.loading) {
+      return <div>Loading</div>;
+    }
+
+    if (this.props.AllUsersQuery && this.props.AllUsersQuery.error) {
+      return <div>Error</div>;
+    }
+
+   const usersToRender = this.props.AllUsersQuery.allUsers
+
+    return (
+      <div>
+        {usersToRender.map((user, index) => {
+          const { firstName, lastName, id } = user
+          return (
+            <div key={id}>
+              <Link
+                to={{
+                  pathname: `/show/${id}`,
+                  state: {
+                    user,
+                  },
+                }}
+              >
+                {`${firstName} ${lastName}`}
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
@@ -11,10 +46,22 @@ class Home extends React.PureComponent {
         <Link to="about">
           Link to about
         </Link>
-        {/* TODO list of users goes here */}
+        {this.renderUsers()}
       </div>
     )
   }
 }
 
-export default Home
+const ALL_USERS_QUERY = gql`
+  query AllUsersQuery {
+    allUsers {
+      createdAt,
+      firstName,
+      lastName,
+      id, 
+      updatedAt
+    }
+  }
+`
+
+export default graphql(ALL_USERS_QUERY, { name: 'AllUsersQuery' })(Home)
